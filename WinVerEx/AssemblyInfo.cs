d50d53f15e821ba87daa7ba17369cebf9e1b7a6c
Randy <randy@randyrants.com>
2019-06-10 10:41:45 -0700

    Initial check in

---
+++ WinVerEx/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WinVerEx")]
+[assembly: AssemblyDescription("Command prompt utility that returns a fully qualified version of Windows 10")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("RandyRants LLC")]
+[assembly: AssemblyProduct("WinVerEx")]
+[assembly: AssemblyCopyright("Copyright © 2019 RandyRants.com")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b3ef68e0-4675-49a2-ac34-2705a43a9fdb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WinVerEx/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Win32;
+
+namespace WinVerEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // All of the data we're looking for lives under the same Registry Key
+            RegistryKey currentVersion = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Windows NT\CurrentVersion");
+
+            // Grab the core of the version number but since minor version is always 1 here, pick up the real minor version from the UBR value
+            String buildLabEx = currentVersion.GetValue("BuildLabEx").ToString();
+            Console.WriteLine("Current version: " + buildLabEx.Replace(".1.", String.Format(".{0}.", currentVersion.GetValue("UBR").ToString())));
+
+            // Grab the product name, as this includes "SKU" information
+            Console.WriteLine("Product name: " + currentVersion.GetValue("ProductName"));
+
+            // This is a deeper dive on what "SKU" you're running
+            Console.WriteLine("Edition ID: " + currentVersion.GetValue("EditionID"));
+
+            // Grab the installation type as well
+            Console.WriteLine("Installation type: " + currentVersion.GetValue("InstallationType"));
+            currentVersion.Close();
+            
+            if (args.Length > 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Visit http://www.github.com/randyrants/winverex for more information or source code.");
+            }
+        }
+    }
+}
